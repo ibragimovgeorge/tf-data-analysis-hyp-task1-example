8d5eb3f66cfd84e7903066caa34d861ec6da0785
ibragimovgeorge <103066850+ibragimovgeorge@users.noreply.github.com>
2023-04-13 22:12:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 201321241 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    return (x_success/x_cnt)>(y_success/y_cnt*1.02) # Ваш ответ, True или False
